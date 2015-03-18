fa091e91a94e3da8f2a612bd1ae4ac242d513482
Ethan <emcpwns@gmail.com>
2015-03-18 16:36:23 -0400

    Update

--- engine.js
+++ engine.js
@@ -1,12 +1,45 @@
-document.write('<p>Your name is ');
-document.write(chance.name({ prefix: false }));
-document.write(' ');
-document.write('and you are ');
-document.write(chance.age({type: 'adult'}));
-document.write(' year old ');
-document.write(chance.weighted(['Human', 'Elf', 'Orc', 'Goblin', 'Imp', 'Nymph', 'Demon', 'Satyr', 'Gnome', 'Troll'], [10, 3, 2, 1, 1, 1, 1, 3, 2, 1]));
-document.write(' ');
-document.write(chance.pick(['Mage', 'Ranger', 'Knight', 'Warrior', 'Bandit', 'Noble', 'Archer']));
-document.write('.</p>');
+/*
+
+      ___           ___         ___                    ___           ___           ___                 
+     /  /\         /  /\       /  /\                  /  /\         /__/\         /  /\          ___   
+    /  /::\       /  /::\     /  /:/_                /  /:/         \  \:\       /  /::\        /  /\  
+   /  /:/\:\     /  /:/\:\   /  /:/ /\              /  /:/           \__\:\     /  /:/\:\      /  /:/  
+  /  /:/~/:/    /  /:/~/:/  /  /:/_/::\            /  /:/  ___   ___ /  /::\   /  /:/~/::\    /  /:/   
+ /__/:/ /:/___ /__/:/ /:/  /__/:/__\/\:\          /__/:/  /  /\ /__/\  /:/\:\ /__/:/ /:/\:\  /  /::\   
+ \  \:\/:::::/ \  \:\/:/   \  \:\ /~~/:/          \  \:\ /  /:/ \  \:\/:/__\/ \  \:\/:/__\/ /__/:/\:\  
+  \  \::/~~~~   \  \::/     \  \:\  /:/            \  \:\  /:/   \  \::/       \  \::/      \__\/  \:\ 
+   \  \:\        \  \:\      \  \:\/:/              \  \:\/:/     \  \:\        \  \:\           \  \:\
+    \  \:\        \  \:\      \  \::/                \  \::/       \  \:\        \  \:\           \__\/
+     \__\/         \__\/       \__\/                  \__\/         \__\/         \__\/                
+
+This engine was originally created by Ethan Cochran (aka emcpwns on Github)
+This engine utilizes Chance.Js which is available for use under an MIT License.
+*/
+
+//Initialize Chance.JS using Random.Org's true random number generator
+var mySeed;
+$.get("https://www.random.org/integers/", {num: "1", col: "1", min: "1", max: "1000000000", base: "10", format: "plain", rnd: "new"}, function(randNum) {
+  mySeed = randNum;
+  var my_seeded_chance = new Chance(mySeed);
+  console.log(my_seeded_chance.natural());
+});
+
+// Create variables
+var uName;  //User's character name
+var uAge;   //User's character age
+var uRace;  //User's character race
+var uClass; //User's character class 
+
+// Generate scenario
+uName = chance.name({ prefix: false });
+uAge = chance.age({type: 'adult'});
+uRace = chance.weighted(['Human', 'Elf', 'Orc', 'Goblin', 'Imp', 'Nymph', 'Demon', 'Satyr', 'Gnome', 'Troll'], [10, 3, 2, 1, 1, 1, 1, 3, 2, 1]);
+uClass = chance.pick(['Mage', 'Ranger', 'Knight', 'Warrior', 'Bandit', 'Noble', 'Archer']);
+
+// Display content
+window.onload = function()
+{
+  document.getElementById("engine").innerHTML = 'Your name is ' + uName + ' and you are a ' + uAge + ' year old ' + uRace + ' ' + uClass + ' .';
+};
 
 
